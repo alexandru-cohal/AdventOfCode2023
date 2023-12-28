b6cfe36de2fab7d8eda26016401bf2934b1b2550
Alexandru Cohal <alexandru.cohal@yahoo.com>
2023-12-28 22:13:11 +0100

    Reimplement day 3 solution for part 1. Add day 3 solution for part 2.

--- day3.py
+++ day3.py
@@ -1,46 +1,84 @@
 import re
 
-def getSumPartNumbers(line):
+def readPart1And2(file):
+    # Reading
+    numberList = []
+    symbolList = []
+    number = 0
+    posStart = posEnd = None
+    lineIndex = -1
+
+    for line in file:
+        lineIndex += 1
+        numberList.append([])
+        symbolList.append([])
+        for elemIndex, elem in enumerate(line):
+            if '0' <= elem <= '9':
+                number = number * 10 + int(elem)
+                if posStart is None:
+                    posStart = elemIndex
+                posEnd = elemIndex
+            else:
+                if number:
+                    numberList[lineIndex].append([number, posStart, posEnd])
+                    posStart = posEnd = None
+                    number = 0
+                if elem not in ['\n', '.']:
+                    symbolList[lineIndex].append([elem, elemIndex])
+
+    return numberList, symbolList
+
+def isPartNumber(line, colStart, colEnd, symbolList):
+    for lineIndex in range(max(0, line-1), min(len(symbolList)-1, line+1) + 1):
+        for symbol in symbolList[lineIndex]:
+            # symbol = symbol[0], pos = symbol[1]
+            if colStart - 1 <= symbol[1] <= colEnd + 1:
+                return True
+    return False
+
+def part1(numberList, symbolList):
     sumPartNumber = 0
-    value = 0
-    isPartNumber = False
 
-    for idxCol, elemCurrent in enumerate(line[1]):
-        if elemCurrent.isdigit():
-            value = value * 10 + int(elemCurrent)
-            if not isPartNumber:
-                for idxLine in [0, 1, 2]:
-                    for idxColOffset in [-1, 0, 1]:
-                        elemNeighb = line[idxLine][idxCol + idxColOffset]
-                        if (not elemNeighb.isdigit() and elemNeighb != '.'):
-                            isPartNumber = True
-        else:
-            if isPartNumber:
-                sumPartNumber += value
-            value = 0
-            isPartNumber = False
-
+    for lineNumberIndex, lineNumber in enumerate(numberList):
+        for number in lineNumber:
+            # value = number[0], posStart = number[1], posEnd = number[2]
+            if isPartNumber(lineNumberIndex, number[1], number[2], symbolList):
+                sumPartNumber += number[0]
     return sumPartNumber
 
-def part1(file):
-    line = ['', '', '']
-    sumPartNumber = 0
+def isGear(line, col, numberList):
+    numAdjPartNumber = 0
+    gearRatio = 1
 
-    for lineRead in file:
-        lineRead = '.' + lineRead[:-1] + '.'
-        line = [line[1], line[2], lineRead]
+    for lineIndex in range(max(0, line-1), min(len(numberList)-1, line+1) + 1):
+        for number in numberList[lineIndex]:
+            # value = number[0], posStart = number[1], posEnd = number[2]
+            if number[1] - 1 <= col <= number[2] + 1:
+                numAdjPartNumber += 1
+                gearRatio *= number[0]
 
-        if line[1]:
-            if not line[0]:
-                line[0] = '.' * len(line[1])
+    if numAdjPartNumber == 2:
+        return gearRatio
+    else:
+        return None
 
-            sumPartNumber += getSumPartNumbers(line)
+def part2(numberList, symbolList):
+    sumGearRatio = 0
 
-    line = [line[1], line[2], '.' * len(line[1])]
-    sumPartNumber += getSumPartNumbers(line)
+    for lineSymbolIndex, lineSymbol in enumerate(symbolList):
+        for symbol in lineSymbol:
+            # symbol = symbol[0], pos = symbol[1]
+            if symbol[0] == '*':
+                gearRatio = isGear(lineSymbolIndex, symbol[1], numberList)
+                if gearRatio:
+                    sumGearRatio += gearRatio
 
-    return sumPartNumber
+    return sumGearRatio
 
 if __name__ == '__main__':
     file = open("inputs/day3.txt", "r")
-    print(part1(file))
+    numberList, symbolList = readPart1And2(file)
+
+    print(part1(numberList, symbolList))
+
+    print(part2(numberList, symbolList))
